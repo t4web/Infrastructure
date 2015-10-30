e512971a0629e00bb56671a6e46e67ae92c7fe05
maxgu <gulturyan@t4web.com.ua>
2015-10-30 09:54:42 +0200

    QueryBuilder

---
+++ src/QueryBuilder.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace T4webInfrastructure;
+
+use Zend\Db\Sql\Select;
+use Zend\Db\Sql\Where;
+use Zend\Db\Sql\Predicate\Predicate;
+use T4webDomainInterface\Infrastructure\CriteriaInterface;
+
+class QueryBuilder
+{
+
+    /**
+     * @var string
+     */
+    protected $tableName;
+
+    public function __construct($tableName)
+    {
+        $this->tableName = $tableName;
+    }
+
+    /**
+     * @param CriteriaInterface $criteria
+     * @return Select
+     */
+    public function getSelect(CriteriaInterface $criteria)
+    {
+        $select = new Select();
+        $select->from($this->tableName);
+        $this->buildPredicate($select, $criteria);
+        $this->buildOrPredicate($select, $criteria);
+        $this->buildOrder($select, $criteria);
+
+        if (!empty($criteria->getLimit())) {
+            $select->limit($criteria->getLimit());
+        }
+
+        if (!empty($criteria->getOffset())) {
+            $select->offset($criteria->getOffset());
+        }
+
+        $this->buildRelations($select, $criteria);
+
+        return $select;
+    }
+
+    private function buildPredicate(Select $select, CriteriaInterface $criteria)
+    {
+        if (empty($criteria->getPredicate())) {
+            return;
+        }
+
+        $where = new Where();
+
+        foreach($criteria->getPredicate() as $predicate) {
+            $method = $predicate['name'];
+            unset($predicate['name']);
+            $predicate['attribute'] = $criteria->getEntityName() . "." . $predicate['attribute'];
+            call_user_func_array([$where, $method], $predicate);
+        }
+
+        $select->where($where);
+    }
+
+    private function buildOrPredicate(Select $select, CriteriaInterface $criteria)
+    {
+        if (empty($criteria->getOr())) {
+            return;
+        }
+
+        $orWhere = new Predicate();
+
+        foreach($criteria->getOr() as $orCriteria) {
+            foreach($orCriteria->getPredicate() as $predicate) {
+                $method = $predicate['name'];
+                unset($predicate['name']);
+                $predicate['attribute'] = $orCriteria->getEntityName() . "." . $predicate['attribute'];
+                call_user_func_array([$orWhere, $method], $predicate);
+            }
+        }
+
+        $select->where($orWhere, Predicate::OP_OR);
+    }
+
+    private function buildRelations(Select $select, CriteriaInterface $criteria)
+    {
+        /** @var CriteriaInterface $relation */
+        foreach ($criteria->getRelations() as $relation) {
+            $select->join(
+                $relation->getEntityName(),
+                sprintf('%s.id = %s.user_id', $criteria->getEntityName(), $relation->getEntityName()),
+                []
+            );
+
+            if (empty($relation->getPredicate())) {
+                comtinue;
+            }
+
+            foreach($relation->getPredicate() as $predicate) {
+                $method = $predicate['name'];
+                unset($predicate['name']);
+                $predicate['attribute'] = $relation->getEntityName() . "." . $predicate['attribute'];
+                call_user_func_array([$select->where, $method], $predicate);
+            }
+
+            //$select->where->addPredicates($predicate, $combination);
+        }
+    }
+
+    private function buildOrder(Select $select, CriteriaInterface $criteria)
+    {
+        if (empty($criteria->getOrder())) {
+            return;
+        }
+
+        $order = [];
+        /** @var CriteriaInterface $relation */
+        foreach ($criteria->getOrder() as $order) {
+            $order = $criteria->getEntityName() . "." . $order;
+        }
+
+        $select->order($order);
+    }
+
+}
---
+++ tests/QueryBuilderTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace T4webDomainTest;
+
+use Zend\Db\Adapter\Adapter;
+use T4webInfrastructure\QueryBuilder;
+use T4webInfrastructure\Criteria;
+
+class QueryBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    private $dbAdapter;
+
+    public function setUp()
+    {
+        $this->dbAdapter = new Adapter([
+            'driver'         => 'Pdo',
+            'dsn'            => 'mysql:dbname=board;host=localhost',
+            'driver_options' => array(
+                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+            ),
+            'username' => 'board',
+            'password' => '111',
+        ]);
+    }
+
+    public function testPredicateGetSelect()
+    {
+        $qb = new QueryBuilder('users');
+
+        $criteria = new Criteria('users');
+        $criteria->equalTo('id', 2);
+        $criteria->notEqualTo('id', 3);
+        $criteria->lessThan('id', 4);
+        $criteria->greaterThan('id', 5);
+        $criteria->greaterThanOrEqualTo('id', 6);
+        $criteria->lessThanOrEqualTo('id', 7);
+        $criteria->like('name', 'php');
+        $criteria->isNull('id');
+        $criteria->isNotNull('id');
+        $criteria->in('type', [1,2,3]);
+        $criteria->between('id', 1, 22);
+        $criteria->limit(4);
+        $criteria->offset(1);
+        $criteria->order('id');
+
+        $select = $qb->getSelect($criteria);
+
+        $sql = $select->getSqlString($this->dbAdapter->getPlatform());
+
+        $this->assertInstanceOf('Zend\Db\Sql\Select', $select);
+        $this->assertEquals(
+            "SELECT `users`.* "
+            . "FROM `users` "
+            . "WHERE `users`.`id` = '2' "
+            . "AND `users`.`id` != '3' "
+            . "AND `users`.`id` < '4' "
+            . "AND `users`.`id` > '5' "
+            . "AND `users`.`id` >= '6' "
+            . "AND `users`.`id` <= '7' "
+            . "AND `users`.`name` LIKE 'php' "
+            . "AND `users`.`id` IS NULL "
+            . "AND `users`.`id` IS NOT NULL "
+            . "AND `users`.`type` IN ('1', '2', '3') "
+            . "AND `users`.`id` BETWEEN '1' AND '22' "
+            . "ORDER BY `users`.`id` ASC "
+            . "LIMIT '4' "
+            . "OFFSET '1'",
+            $sql
+        );
+    }
+
+    public function testRelationGetSelect()
+    {
+        $qb = new QueryBuilder('users');
+
+        $criteria = new Criteria('users');
+        $criteria->greaterThan('id', 5);
+        $criteria->limit(20);
+        $criteria->order('id');
+        $criteria->relation('photos')
+            ->in('status', [2,3]);
+
+        $select = $qb->getSelect($criteria);
+
+        $sql = $select->getSqlString($this->dbAdapter->getPlatform());
+
+        $this->assertInstanceOf('Zend\Db\Sql\Select', $select);
+        $this->assertEquals(
+            "SELECT `users`.* "
+            . "FROM `users` "
+            . "INNER JOIN `photos` ON `users`.`id` = `photos`.`user_id` "
+            . "WHERE `users`.`id` > '5' "
+            . "AND `photos`.`status` IN ('2', '3') "
+            . "ORDER BY `users`.`id` ASC "
+            . "LIMIT '20'",
+            $sql
+        );
+    }
+
+    public function testOrGetSelect()
+    {
+        $qb = new QueryBuilder('users');
+
+        $criteria = new Criteria('users');
+        $criteria->greaterThan('id', 5);
+        $criteria->orCriteria()
+            ->lessThan('id', 50)
+            ->notEqualTo('id', 40);
+        $criteria->limit(20);
+        $criteria->order('id');
+        $criteria->relation('photos')
+            ->in('status', [2,3]);
+
+        $select = $qb->getSelect($criteria);
+
+        $sql = $select->getSqlString($this->dbAdapter->getPlatform());
+
+        $this->assertInstanceOf('Zend\Db\Sql\Select', $select);
+        $this->assertEquals(
+            "SELECT `users`.* "
+            . "FROM `users` "
+            . "INNER JOIN `photos` ON `users`.`id` = `photos`.`user_id` "
+            . "WHERE `users`.`id` > '5' "
+            . "OR (`users`.`id` < '50' AND `users`.`id` != '40') "
+            . "AND `photos`.`status` IN ('2', '3') "
+            . "ORDER BY `users`.`id` ASC "
+            . "LIMIT '20'",
+            $sql
+        );
+    }
+
+}
