dd2555649f4ccbaf41abd71cb894d7c2babb67a2
maxgu <maxgu@t4web.com.ua>
2015-11-03 22:21:51 +0200

    update travis config

--- .ci/mysql_fixtures.sh
+++ .ci/mysql_fixtures.sh
@@ -6,7 +6,7 @@ mysql -u root -e 'CREATE USER \'board\'@\'localhost\' IDENTIFIED BY \'111\';'
 mysql -u root -e 'GRANT ALL PRIVILEGES ON *.* TO \'board\'@\'localhost\';'
 mysql -u root -e 'FLUSH PRIVILEGES;'
 mysql -u root -e 'CREATE DATABASE board;'
-mysql -u root -e 'CREATE TABLE IF NOT EXISTS `tasks` (
+mysql -u root board -e 'CREATE TABLE IF NOT EXISTS `tasks` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(255) NOT NULL,
 `assignee` varchar(2) NOT NULL,
@@ -14,7 +14,7 @@ mysql -u root -e 'CREATE TABLE IF NOT EXISTS `tasks` (
 `type` tinyint(1) NOT NULL,
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
-mysql -u root -e 'INSERT INTO `tasks` (`id`, `name`, `assignee`, `status`, `type`) VALUES
+mysql -u root board -e 'INSERT INTO `tasks` (`id`, `name`, `assignee`, `status`, `type`) VALUES
 (2, 'qq', 'qq', 1, 1),
 (3, '194556', '45', 0, 0),
 (4, 'Some name', 'AA', 0, 0);'
