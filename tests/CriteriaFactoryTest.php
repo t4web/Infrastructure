075a1ae134c2fd21d8be88899ab2768736eadd57
maxgu <gulturyan@t4web.com.ua>
2016-03-01 13:16:06 +0200

    update CriteriaFactoryTest

--- tests/CriteriaFactoryTest.php
+++ tests/CriteriaFactoryTest.php
@@ -62,5 +62,29 @@ class CriteriaFactoryTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf('T4webDomainInterface\Infrastructure\CriteriaInterface', $criteria);
         $this->assertEquals('Task', $criteria->getEntityName());
+
+        /** @var \Zend\Db\Sql\Select $select */
+        $select = $criteria->getQuery();
+
+        $dbAdapter = new \Zend\Db\Adapter\Adapter([
+            'driver'         => 'Pdo',
+            'dsn'            => 'mysql:dbname=board;host=localhost',
+            'driver_options' => [
+                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+            ],
+            'username' => 'board',
+            'password' => '111',
+        ]);
+
+        $this->assertEquals(
+            "SELECT `tasks`.*"
+            . " FROM `tasks`"
+            . " INNER JOIN `users` ON `tasks`.`user_id` = `users`.`id`"
+            . " WHERE `tasks`.`status` = '2'"
+            . " AND `tasks`.`date_create` > '2015-10-30'"
+            . " AND `users`.`status` IN ('2', '3', '4')"
+            . " AND `users`.`name` LIKE 'gor'",
+            $select->getSqlString($dbAdapter->getPlatform())
+        );
     }
 }
