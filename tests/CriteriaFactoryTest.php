1f5d49bf28746e064218f5e01f929b7b54144e7b
maxgu <gulturyan@t4web.com.ua>
2016-03-01 13:04:41 +0200

    update CriteriaFactoryTest

--- tests/CriteriaFactoryTest.php
+++ tests/CriteriaFactoryTest.php
@@ -4,7 +4,6 @@ namespace T4webInfrastructureTest;
 
 use T4webInfrastructure\CriteriaFactory;
 use T4webInfrastructure\Config;
-use Zend\Stdlib\ArrayObject;
 
 class CriteriaFactoryTest extends \PHPUnit_Framework_TestCase
 {
@@ -14,11 +13,6 @@ class CriteriaFactoryTest extends \PHPUnit_Framework_TestCase
     private $criteriaFactory;
 
     public function setUp()
-    {
-        $this->criteriaFactory = new CriteriaFactory();
-    }
-
-    public function testBuild()
     {
         $config = new Config(
             [
@@ -46,9 +40,13 @@ class CriteriaFactoryTest extends \PHPUnit_Framework_TestCase
             ]
         );
 
+        $this->criteriaFactory = new CriteriaFactory($config);
+    }
+
+    public function testBuild()
+    {
         $criteria = $this->criteriaFactory->build(
             'Task',
-            $config,
             [
                 'status.equalTo' => 2,
                 'dateCreate.greaterThan' => '2015-10-30',
@@ -64,39 +62,5 @@ class CriteriaFactoryTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf('T4webDomainInterface\Infrastructure\CriteriaInterface', $criteria);
         $this->assertEquals('Task', $criteria->getEntityName());
-        $this->assertEquals(
-            [
-                [
-                    'name' => 'equalTo',
-                    'attribute' => 'status',
-                    'value' => 2,
-                ],
-                [
-                    'name' => 'greaterThan',
-                    'attribute' => 'dateCreate',
-                    'value' => '2015-10-30',
-                ],
-            ],
-            $criteria->getPredicate()
-        );
-
-        $relations = $criteria->getRelations();
-
-        $this->assertEquals('User', $relations[0]->getEntityName());
-        $this->assertEquals(
-            [
-                [
-                    'name' => 'in',
-                    'attribute' => 'status',
-                    'values' => [2, 3, 4],
-                ],
-                [
-                    'name' => 'like',
-                    'attribute' => 'name',
-                    'value' => 'gor',
-                ],
-            ],
-            $relations[0]->getPredicate()
-        );
     }
 }
