204e07e3338e408005dd92fa66ed1fde1b500408
Slavik <spam-slavik@i.ua>
2017-11-14 18:23:23 +0200

    fix: Bug when id is null

--- src/Repository.php
+++ src/Repository.php
@@ -104,7 +104,7 @@ class Repository implements RepositoryInterface
     {
         $id = $entity->getId();
 
-        if ($this->identityMap->offsetExists((int)$id)) {
+        if (!is_null($id) && $this->identityMap->offsetExists((int)$id)) {
             if (!$this->isEntityChanged($entity)) {
                 return;
             }
